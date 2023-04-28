e43881787a5d4117c12ca981db641a5b4b7e3a4c
realtanwei <everal@yeah.net>
2023-04-28 17:02:33 +0800

    添加SpringCloud相关依赖

---
+++ spring-cloud-business/spring-cloud-consumer/src/main/java/com/tanwei/spring/business/consumer/ConsumerSpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.tanwei.spring.business.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tanwei
+ * @date 2023-04-28 16:24
+ **/
+@SpringBootApplication
+public class ConsumerSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerSpringBootApplication.class, args);
+    }
+}
